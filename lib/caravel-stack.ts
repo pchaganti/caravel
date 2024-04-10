4295d4cdfcf393c3f788c3e3cb7f1a5e507f45fa
Prabhakar Chaganti <prabhakar@ylastic.com>
2024-04-10 10:52:58 -0400

    Create an empty bp stack and setup context and paths.

--- lib/caravel-stack.ts
+++ lib/caravel-stack.ts
@@ -1,16 +1,21 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as blueprints from "@aws-quickstart/eks-blueprints";
+
+blueprints.HelmAddOn.validateHelmVersions = true;
+
+const addOns: Array<blueprints.ClusterAddOn> = [];
 
 export class CaravelStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CaravelQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const stack = blueprints.EksBlueprint.builder()
+      .account(props?.env?.account)
+      .region(props?.env?.region)
+      .version(this.node.tryGetContext("caravel.eks.version"))
+      .addOns(...addOns)
+      .useDefaultSecretEncryption(true)
+      .build(scope, `${id}-stack`);
   }
 }
