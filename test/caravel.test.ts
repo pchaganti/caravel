9bca81d96d2d008eba96a60dd6e0906b2c647423
Prabhakar Chaganti <prabhakar@ylastic.com>
2024-04-10 09:45:55 -0400

    Initial cdk project from TS template.

---
+++ test/caravel.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Caravel from '../lib/caravel-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/caravel-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Caravel.CaravelStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
